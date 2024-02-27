6faaeaebda381bfaa13934f0dce44823169d998e
ItsAhmad <ahmad.ameen526@hotmail.com>
2024-02-27 08:07:15 -0500

    AWSD keys console.logging

--- input.js
+++ input.js
@@ -1,10 +1,23 @@
-
+let connection;
 
 const handleUserInput = function (key) {
   if (key === '\u0003') { 
     console.log("Quitting game");
     process.exit();
   }
+
+  if (key === 'w') {
+    console.log("Move: up");
+  } 
+  if (key === 'a') {
+    console.log("Move: left");
+  } 
+  if (key === 's') {
+    console.log("Move: down");
+  } 
+  if (key === 'd') {
+    console.log("Move: right");
+  } 
 };
 
 const setupInput = function () {
@@ -14,6 +27,10 @@ const setupInput = function () {
   stdin.resume();
   stdin.on("data", handleUserInput);
   return stdin;
+};
+
+const setupInput = (conn) => {
+  connection = conn;
 };
 
 module.exports = { setupInput };
