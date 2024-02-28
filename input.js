36ccc97b8be64de5a4d826b7d6d705d7f6864cf5
ItsAhmad <ahmad.ameen526@hotmail.com>
2024-02-28 08:38:03 -0500

    cleaned up code with eslint

--- input.js
+++ input.js
@@ -1,23 +1,23 @@
 let connection;
 
-const handleUserInput = function (key) {
-  if (key === '\u0003') { 
+const handleUserInput = function(key) {
+  if (key === '\u0003') {
     console.log("Quitting game");
     process.exit();
   }
 
   if (key === 'w') {
     connection.write("Move: up");
-  } 
+  }
   if (key === 'a') {
     connection.write("Move: left");
-  } 
+  }
   if (key === 's') {
     connection.write("Move: down");
-  } 
+  }
   if (key === 'd') {
     connection.write("Move: right");
-  } 
+  }
 };
 
 const setupInput = (conn) => {
